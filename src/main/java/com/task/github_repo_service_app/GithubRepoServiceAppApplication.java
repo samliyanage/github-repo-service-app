f12a4d478c7e1f64fb464fb99daa8781af8111f8
Sampath Liyanage <sampathliyanage@hotmail.com>
2024-08-19 22:39:24 +0800

    Project initial setup

---
+++ src/main/java/com/task/github_repo_service_app/GithubRepoServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.task.github_repo_service_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepoServiceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/github_repo_service_app/GithubRepoServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.github_repo_service_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepoServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
